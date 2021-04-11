e0b33c5b02ecf093a2bfa942a1fd077593a88536
Igor Braga <higorb1@gmail.com>
2021-04-11 17:35:42 -0400

    Clean main and allow warp and Rocket HTTP servers to run
    Signed-off-by: Igor Braga <higorb1@gmail.com>

--- README.md
+++ README.md
@@ -22,3 +22,14 @@ Run the server
 ```
 cargo +nightly run
 ```
+
+Accessing GraphQL Playground to make requests.
+If using `warp` server access:
+```
+http://127.0.0.1:3030/graphiql
+```
+
+If using `Rocket` server access:
+```
+http://127.0.0.1:8080
+```
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,15 @@
 
 mod graphql_schema;
 
-// #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket;
 
+use juniper_rocket;
+use juniper_warp;
+use rocket::{response::content, State};
+use warp::{Filter};
 use juniper::{
     EmptySubscription, EmptyMutation,
-  };
-// use rocket::{response::content, State};
-// use juniper_rocket;
-use juniper_warp;
-
-use warp::{Filter};
+};
 
 fn schema() -> graphql_schema::Schema {
     graphql_schema::Schema::new(
@@ -24,8 +23,28 @@ fn schema() -> graphql_schema::Schema {
     )
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
+    let mut user_input = String::new();
+    println!("Choose which server you'd like to run:\n[1] warp\n[2] Rocket");
+    std::io::stdin().read_line(&mut user_input).expect("Failed");
+    // Cast String input into i32 type
+    let user_choice: i32 = user_input.trim().parse().expect("Failed to convert user input to i32");
+
+    if 1 == user_choice {
+        println!("Hello from warp server\nListening on http://127.0.0.1:3030. Please visit http://127.0.0.1:3030/graphiql to access the GraphQL playground.");
+        let mut rt = tokio::runtime::Runtime::new().unwrap();
+        let future = warp_server();
+        rt.block_on(future);
+    } else if 2 == user_choice {
+        println!("Hello from Rocket server\nListening on http://127.0.0.1:8080. Please visit http://127.0.0.1:8080 to access the GraphQL playground.");
+        rocket_server();
+    } else {
+        println!("Wrong input, please choose either [1,2]");
+    }
+    
+}
+
+async fn warp_server() {
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::get()
         .and(warp::path("hello").and(warp::path::param())
@@ -35,7 +54,7 @@ async fn main() {
         .and(warp::path("graphiql"))
         .and(juniper_warp::graphiql_filter("/graphql", None));
 
-    let state = warp::any().map(move || () ).boxed();
+    let state = warp::any().map(move || () ).boxed(); // TODO: Do we need this?
     let graphql_filter = warp::path("graphql")
         .and(juniper_warp::make_graphql_filter(schema(), state));
 
@@ -48,27 +67,27 @@ async fn main() {
         .await;
 }
 
-// #[rocket::get("/")]
-// fn graphiql() -> content::Html<String> {
-//     juniper_rocket::graphiql_source("/graphql", None)
-// }
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket::graphiql_source("/graphql", None)
+}
 
-// #[rocket::post("/graphql", data = "<request>")]
-// fn post_graphql_handler(
-//     _context: State<graphql_schema::Schema>,
-//     request: juniper_rocket::GraphQLRequest,
-//     schema: State<graphql_schema::Schema>,
-// ) -> juniper_rocket::GraphQLResponse {
-//     request.execute_sync(&schema, &())
-// }
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    _context: State<graphql_schema::Schema>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<graphql_schema::Schema>,
+) -> juniper_rocket::GraphQLResponse {
+    request.execute_sync(&schema, &())
+}
 
-// fn main() {
-//     rocket::ignite()
-//         .manage(graphql_schema::Schema::new(
-//             graphql_schema::Query,
-//             EmptyMutation::<()>::new(),
-//             EmptySubscription::<()>::new(),
-//         ))
-//         .mount("/", routes![graphiql,post_graphql_handler])
-//         .launch();
-// }
+fn rocket_server() {
+    rocket::ignite()
+        .manage(graphql_schema::Schema::new(
+            graphql_schema::Query,
+            EmptyMutation::<()>::new(),
+            EmptySubscription::<()>::new(),
+        ))
+        .mount("/", routes![graphiql,post_graphql_handler])
+        .launch();
+}
